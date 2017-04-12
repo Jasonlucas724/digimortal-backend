a8ca3447e09ddb7fa00869ef71c7376ad0fa6fbc
theClubhou.se <heythere@theclubhou.se>
2017-04-12 16:05:38 -0400

    commit4

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -10,9 +10,13 @@ class ArticlesController extends Controller
 {
   public function index()
   {
+    $articles = Article::all();
 
-
+    return Response::json($articles);
   }
+
+
+
     //
   public function store(Request $request)
   {
@@ -36,16 +40,44 @@ class ArticlesController extends Controller
 
 
   }
+  //Finds a specific Article based on id
   public function update($id, Request $request)
   {
+    //Saves the title.
+    $article= Article::find($id);
+    //Saves the title.
+    $article->title = $request->input('title');
+    //Saves the body.
+    $article->title = $request->input('body');
 
+    //Moves image to the server and saves the image url to the DB.
+    $image = $request->file('image');
+    $imageName = $image->getClientOriginalName();
+    $image->move("storage/", $imageName);
+    $article->image = $request->root()."/storage/".$imageName;
+
+    //Commits the saves to the database.
+    $article->save();
+
+    //Sends a message back to the front end.
+    return Response::json(["success" => "Article Updated."]);
   }
+
+
   public function show($id)
   {
+    $article = Article::find($id);
+
+    return Response::json($article);
 
   }
   public function destroy($id)
   {
+    $article = Article::find($id);
+
+    $article->delete();
+
+    return Response::json(['success' => 'Deleted Article.']);
 
   }
 
